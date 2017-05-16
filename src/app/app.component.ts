1e8b210e6a6c47f51333d9f577f295c259e04a47
Eldow <francois.decan@free.fr>
2017-05-16 16:57:21 -0400

    Added logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome on Youmuu';
+  description = 'Youmuu is a place where you can build awesome playlists from youtube and share them with your friends.'
 }
